ccc6aa7eff5fc30d5b0c3845f2b2226018719572
Nikolay Gorlov <gnv@ctmed.ru>
2014-04-24 03:39:45 +0700

    insb-1-3 implemented

--- insb-1-3.py
+++ insb-1-3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# insb-1-2.py - обработка ответов из ТФОМС на ЗСП
+# insb-1-3.py - обработка ответов из ТФОМС на ЗСП
 #               (MySQL: mis.sm - файлы ST22M*.csv)
+#               приписываем enp в DBMIS (peoples)
 #
 
 import logging
@@ -14,7 +15,7 @@ insorgs = InsorgInfoList()
 
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 
-LOG_FILENAME = '_insb1_2.out'
+LOG_FILENAME = '_insb1_3.out'
 logging.basicConfig(filename=LOG_FILENAME,level=logging.INFO,)
 
 console = logging.StreamHandler()
@@ -27,25 +28,38 @@ STEP = 1000
 
 UPDATE            = True
 
-SQLT_TPR = """SELECT tp.id, tp.people_id, tp.clinic_id,
-sm.enp, sm.mcod
-FROM tfoms_peoples tp 
-LEFT JOIN sm ON tp.people_id = sm.people_id
-WHERE date_from_tfoms is Null;"""
+#SQLT_SMR = """SELECT people_id, enp
+#FROM sm 
+#WHERE enp_2_dbmis is Null
+#LIMIT 100;"""
 
-SQLT_TPU = """UPDATE tfoms_peoples
+SQLT_SMR = """SELECT people_id, enp
+FROM sm 
+WHERE enp_2_dbmis is Null;"""
+
+SQLT_SMU = """UPDATE sm
 SET
-date_from_tfoms = %s,
-enp = %s,
-lpu_tfoms = %s
+enp_2_dbmis = 1
 WHERE
-id = %s;"""
+people_id = %s;"""
+
+CLINIC_ID = 22
+HOST = "fb2.ctmed.ru"
+DB   = "DBMIS"
+
+SQLT_PEOPLESU = """UPDATE peoples
+SET
+enp = ?
+WHERE
+people_id = ?;"""
+
 
 if __name__ == "__main__":
     
     import os, shutil
     import time
     from dbmysql_connect import DBMY
+    from dbmis_connect2 import DBMIS
 
     import datetime    
 
@@ -53,64 +67,54 @@ if __name__ == "__main__":
     sdnow = str(dnow)
     
 
-    log.info("======================= INSB-1-2 ===========================================")
+    log.info("======================= INSB-1-3 ===========================================")
     localtime = time.asctime( time.localtime(time.time()) )
-    log.info('Filling out TFOMS_PEOPLES table. Start {0}'.format(localtime))
+    log.info('Put ENP into DBMIS. Start {0}'.format(localtime))
     
     dbmy = DBMY()
     curr = dbmy.con.cursor()
     curw = dbmy.con.cursor()
     
-    curr.execute(SQLT_TPR)
+    curr.execute(SQLT_SMR)
     results = curr.fetchall()
 
+    sout = "Database: {0}:{1}".format(HOST, DB)
+    log.info(sout)
+
+    dbc = DBMIS(CLINIC_ID, mis_host = HOST, mis_db = DB)
+    cur = dbc.con.cursor()
 
     nall = 0
     nfound = 0
-    nomcod = 0
     for rec in results:
         nall += 1
-        _id  = rec[0]
-        p_id = rec[1]
-        c_id = rec[2]
-        enp  = rec[3]
-        mcod = rec[4]
+        p_id = rec[0]
+        enp  = rec[1]
 	
 	if nall % STEP == 0:
-	    sout = " {0}: people_id: {1} enp: {2} mcod: {3}".format(nall, p_id, enp, mcod)
+	    sout = " {0}: people_id: {1} enp: {2}".format(nall, p_id, enp)
 	    log.info(sout)
 	    
         if enp is None:
             nfound += 1
             continue
-	if mcod is not None:
-	    try:
-		mo = modb[mcod]
-		lpu_tfoms = mo.mis_code
-	    except:
-		sout = "id: {0} people_id: {1}. Clinic not found for mcod = {2}".format(_id, p_id, mcod)
-		log.warn(sout)
-		lpu_tfoms = None
-		nomcod += 1
-	else:
-	    lpu_tfoms = None
-	    nomcod += 1
+	
+	cur.execute(SQLT_PEOPLESU, (enp, p_id, ))
+	dbc.con.commit()
 	    
-	curw.execute(SQLT_TPU,(dnow, enp, lpu_tfoms, _id, ))
+	curw.execute(SQLT_SMU,( p_id, ))
 	dbmy.con.commit()
 	
     localtime = time.asctime( time.localtime(time.time()) )
-    log.info('Filling out TFOMS_PEOPLES table. Finish  '+localtime)  
+    log.info('Put ENP into DBMIS. Finish  '+localtime)  
     
     sout = "Totally {0} records have been processed".format(nall)
     log.info(sout)
 
-    sout = " {0} people_id have not been found in the sm table".format(nfound)
+    sout = " {0} people_id have not got ENP in the sm table".format(nfound)
     log.info(sout)
 
-    sout = " {0} people_id have not got lpu_tfoms".format(nomcod)
-    log.info(sout)
-    
+    dbc.close()
     dbmy.close()
     sys.exit(0)    
         
