8c8c039cddff8b08e33dc5fbbdbc0a17e85471a3
Nikolay Gorlov <gnv@ctmed.ru>
2014-06-03 19:09:41 +0700

    minor adjustment

--- radar-1.py
+++ radar-1.py
@@ -154,15 +154,16 @@ if __name__ == '__main__':
 
     font = {'family': 'Droid Sans',
 	    'weight': 'normal',
-	    'size': 12}
+	    'size': 13}
     
     rc('font', **font)
 
     N = 12
     theta = radar_factory(N, frame='polygon')
 
-    spoke_labels = [u'Январь', u'Февраль', u'Март', u'Апрель', u'Май', u'Июнь', u'Июль', u'Август',
-             u'Сентябрь', u'Октябрь', u'Ноябрь', u'Декабрь']
+    spoke_labels = [u'Январь', u'Февраль', u'Март', u'Апрель', 
+                    u'Май', u'Июнь', u'Июль', u'Август',
+                    u'Сентябрь', u'Октябрь', u'Ноябрь', u'Декабрь']
 
     a = get_data()
     #print a
