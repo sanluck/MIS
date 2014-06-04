f80c5f11fbb49ed0dd5373946327ad8131d23089
Nikolay Gorlov <gnv@ctmed.ru>
2014-06-04 08:33:28 +0700

    em-3g - Graph em-3 data

--- em-3g.py
+++ em-3g.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# em-3.py - выборка пациентов с заданными диагнозами
+# em-3g.py - выборка пациентов с заданными диагнозами
 #               по списку ЛПУ
 #               за указанный период времени
-#               вывод в em3.xls
+#               отображение по дням на графиках
 #
 
 import logging
@@ -11,7 +11,7 @@ import sys, codecs
 
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 
-LOG_FILENAME = '_em3.out'
+LOG_FILENAME = '_em3g.out'
 logging.basicConfig(filename=LOG_FILENAME,level=logging.INFO,)
 
 console = logging.StreamHandler()
@@ -31,6 +31,8 @@ STEP = 1000
 
 D_LIST = ['A01%', 'A02%', 'A03%', 'A04%', 'A08%', 'A09%', 'B15%', 'J03%', 'J06%']
 C_LIST = [132, 106, 139, 149, 150, 162, 217, 171, 202, 173, 127, 181]
+
+F_PATH = "./EM3"
 F_NAME = "em3.xls"
 
 s_sqlt1 = """SELECT 
@@ -50,6 +52,7 @@ if __name__ == "__main__":
     from datetime import datetime
     from dbmis_connect2 import DBMIS
     import xlwt
+    import matplotlib.pyplot as plt
  
     localtime = time.asctime( time.localtime(time.time()) )
     log.info('--------------------------------------------------------------------')
@@ -76,7 +79,7 @@ if __name__ == "__main__":
 
     row = 3
 
-    col = 0
+    col = 1
     for ddd in D_LIST:
 	col += 1
 	ws.write(row,col,ddd)
@@ -88,11 +91,12 @@ if __name__ == "__main__":
 	cname = dbc.name.encode('utf-8')
 	sout = "clinic_id: {0} clinic_name: {1} ".format(clinic_id, cname)
 	log.info(sout)
-	ws.write(row,0,dbc.name)
+	ws.write(row,0,clinic_id)
+	ws.write(row,1,dbc.name)
 	
 	curr = dbc.con.cursor()
 	nnn = 0
-	col = 0
+	col = 1
 	for ddd in D_LIST:
 	    col += 1
 	    curr.execute(s_sqlt1, (d_begin, d_end, ddd, clinic_id, ))
@@ -101,6 +105,34 @@ if __name__ == "__main__":
 	    sout = "Count[{0}]: {1}".format(ddd, nddd)
 	    log.info(sout)
 	    ws.write(row,col,nddd)
+	    arr_x = []
+	    arr_y = []
+	    d_count = 0
+	    visit_date0 = None
+	    for rec in recs:
+		visit_date = rec[3]
+		if visit_date0 is None:
+		    visit_date0 = visit_date
+		    d_count += 1
+		elif visit_date == visit_date0:
+		    d_count += 1
+		else:
+		    month = visit_date0.month
+		    day = visit_date0.day
+		    n_date = month+day*0.01
+		    arr_x.append(n_date)
+		    arr_y.append(d_count)
+		    d_count = 0
+		    visit_date0 = visit_date
+	    
+	    if nddd > 0:
+		fig = plt.figure(figsize=(9, 7))
+		plt.plot(arr_x, arr_y)
+		plt.ylabel(ddd)
+		plt.xlabel('Date')
+		f_name = F_PATH + "/" + "em3-{0}-{1}.png".format(clinic_id, ddd)
+		plt.savefig(f_name)
+	    
 	    nnn += nddd
 	sout = "Subtotal: {0}".format(nnn)
 	log.info(sout)
@@ -113,7 +145,8 @@ if __name__ == "__main__":
     log.info(sout)
     row += 1
     ws.write(row,col,NNN)
-    wb.save(F_NAME)
+    f_fname = F_PATH + "/" + F_NAME
+    wb.save(f_fname)
     
     localtime = time.asctime( time.localtime(time.time()) )
     log.info('EM 3 Finish  '+localtime)    
