6b08d9d62dd1e8235a5b5b52cc65c4e0bf16b805
Nikolay Gorlov <gnv@ctmed.ru>
2014-07-28 12:47:59 +0700

    minor adjustments for https://prm.ctmed.ru/redmine/issues/510

--- insb-2.py
+++ insb-2.py
@@ -55,34 +55,34 @@ OCATO      = '01000'
 PRINT2     = False
 
 #DATE_RANGE = None
-DATE_RANGE = ['2014-06-01','2014-06-30']
+DATE_RANGE = ['2014-07-01','2014-07-31']
 PRINT_ALL  = True # include all patients into MO files
 
 def get_clist(db):
-    
+
     if MLIST:
 	s_sql = "SELECT DISTINCT mcod FROM mlist WHERE done is Null;"
     else:
 	s_sql = "SELECT DISTINCT mcod FROM insr_list WHERE done is Null;"
-    
+
     cur = db.con.cursor()
     cur.execute(s_sql)
     result = cur.fetchall()
-    
+
     ar = []
-    
+
     for rec in result:
 	mcod = rec[0]
 	ar.append(mcod)
-	
+
     return ar
 
 def register_cdone(db, clinic_id):
-    import datetime    
+    import datetime
 
     dnow = datetime.datetime.now()
     sdnow = str(dnow)
-    
+
     if MLIST:
 	s_sql = """UPDATE mlist
 	SET done = %s
@@ -91,8 +91,8 @@ def register_cdone(db, clinic_id):
 	s_sql = """UPDATE insr_list
 	SET done = %s
 	WHERE clinic_id = %s;"""
-	
-    
+
+
     cur = db.con.cursor()
     cur.execute(s_sql, (sdnow, clinic_id))
 
@@ -101,14 +101,14 @@ def plist(dbc, clinic_id, mcod, patient_list):
     import xlwt
     from PatientInfo import p1, p2
     from insorglist import InsorgInfoList
-    
-    import os    
+
+    import os
     import datetime
     import time
 
     dbmy = DBMY()
     curr = dbmy.con.cursor()
-    
+
     s_sqlf = """SELECT oms_series, oms_number, enp, mcod, ocato, smo_code
     FROM
     sm
@@ -116,7 +116,7 @@ def plist(dbc, clinic_id, mcod, patient_list):
 
     cur = dbc.con.cursor()
 
-    s_sql_ap = """SELECT 
+    s_sql_ap = """SELECT
 ap.area_people_id, ap.area_id_fk, ap.date_beg, ap.motive_attach_beg_id_fk,
 ca.clinic_id_fk
 FROM area_peoples ap
@@ -125,7 +125,7 @@ LEFT JOIN clinic_areas ca ON ar.clinic_area_id_fk = ca.clinic_area_id
 WHERE ap.people_id_fk = ? AND ca.basic_speciality = 1
 AND ap.date_end is Null
 ORDER BY ap.date_beg DESC;"""
-    
+
     insorgs = InsorgInfoList()
 
     stime  = time.strftime("%Y%m%d")
@@ -142,16 +142,16 @@ ORDER BY ap.date_beg DESC;"""
 
     wb = xlwt.Workbook(encoding='cp1251')
     ws = wb.add_sheet('Patients')
-    
+
     sout = u"clinic_id: {0}".format(clinic_id)
     ws.write(0,0,sout)
     ws.write(2,0,u"id")
     ws.write(2,1,u"date_beg")
     ws.write(2,2,u"motive")
     ws.write(2,3,u"clinic_id")
-    
+
     ws_row = 3
-    
+
     count    = 0
     count_e  = 0
     count_a  = 0
@@ -164,9 +164,9 @@ ORDER BY ap.date_beg DESC;"""
     for p_obj in patient_list:
 
 	p_id = p_obj.people_id
-	
+
 	count += 1
-	
+
 	insorg_id   = p_obj.insorg_id
 	try:
 	    insorg = insorgs[insorg_id]
@@ -219,10 +219,10 @@ ORDER BY ap.date_beg DESC;"""
 		if not PRINT_ALL: continue
 	    else:
 		count_b += 1
-		
+
 	cur.execute(s_sql_ap,(p_id, ))
 	recs_ap = cur.fetchall()
-		
+
 	l_print = False
 	if (len(recs_ap) == 1):
 	    if (f_ocato == OCATO):
@@ -242,7 +242,7 @@ ORDER BY ap.date_beg DESC;"""
 		if PRINT2:
 		    sout = "people_id: {0} date_beg: {1} motive_attach: {2} clinic_id: {3}".format(p_id, date_beg, motive_attach, clinic_id_fk)
 		    log.info( sout )
-			
+
 		ws_row += 1
 		ws.write(ws_row,0,p_id)
 		if date_beg is None:
@@ -252,19 +252,19 @@ ORDER BY ap.date_beg DESC;"""
 		ws.write(ws_row,1,s_date_beg)
 		ws.write(ws_row,2,motive_attach)
 		ws.write(ws_row,3,clinic_id_fk)
-			
+
 		if motive_attach in (None, 3, 9):
 		    motive_attach = 2
-			
+
 		if (motive_attach in (2,3)) and (clinic_id == clinic_id_fk) and (not l_print) and (f_ocato == OCATO):
 		    sss = p2(p_obj, mcod, 2, date_beg) + "\r\n"
 		    ps = sss.encode('windows-1251')
 		    l_print = True
-			    
-		
+
+
 	if l_print:
 	    fob.write(ps)
-    
+
 	    fob.flush()
 	    os.fsync(fob.fileno())
 	else:
@@ -289,7 +289,7 @@ ORDER BY ap.date_beg DESC;"""
     # write results into MySQL DB
     dnow = datetime.datetime.now()
     sdnow = str(dnow)
-    
+
     s_sql = """SELECT id FROM iba WHERE clinic_id = %s;"""
     curr.execute(s_sql,(clinic_id,))
     rec = curr.fetchone()
@@ -303,7 +303,7 @@ ORDER BY ap.date_beg DESC;"""
     else:
 	_id = rec[0]
 	s_sql = """UPDATE iba
-	SET 
+	SET
 	done = %s,
 	count = %s,
 	count_e = %s,
@@ -333,7 +333,7 @@ def pclinic(clinic_id, mcod):
 
     cogrn = CLINIC_OGRN.encode('utf-8')
     cname = dbc.name.encode('utf-8')
-    
+
     sout = "clinic_id: {0} cod_mo: {1} clinic_name: {2} clinic_ogrn: {3}".format(clinic_id, mcod, cname, cogrn)
     log.info(sout)
 
@@ -357,33 +357,33 @@ AND ap.date_beg >= '{1}'
 AND ap.date_beg <= '{2}'
 AND ap.date_end is Null;"""
 	s_sql = s_sqlt.format(clinic_id, D1, D2)
-	
+
     cursor = dbc.con.cursor()
     cursor.execute(s_sql)
     results = cursor.fetchall()
-    
+
     localtime = time.asctime( time.localtime(time.time()) )
     log.info('Patients has been selected {0}'.format(localtime))
-    
+
     patient_list = get_patient_list(results)
-    
+
     npatients = len(patient_list)
     sout = "clinic has got {0} unique patients".format(npatients)
     log.info( sout )
-    
+
     plist(dbc, clinic_id, mcod, patient_list)
-    
+
     dbc.close()
     localtime = time.asctime( time.localtime(time.time()) )
     log.info('Insurance Belongings Analysis Finish  '+localtime)
 
 def get_1clinic_lock(id_unlock = None):
-    import datetime    
+    import datetime
     dnow = datetime.datetime.now()
-    
+
     dbmy = DBMY()
     curm = dbmy.con.cursor()
-    
+
     if id_unlock is not None:
 	ssql = "UPDATE insr_list SET done = %s, c_lock = Null WHERE id = %s;"
 	curm.execute(ssql, (dnow, id_unlock, ))
@@ -392,7 +392,7 @@ def get_1clinic_lock(id_unlock = None):
     ssql = "SELECT id, clinic_id, mcod FROM insr_list WHERE (done is Null) AND (c_lock is Null);"
     curm.execute(ssql)
     rec = curm.fetchone()
-    
+
     if rec is not None:
 	_id  = rec[0]
 	c_id = rec[1]
@@ -408,27 +408,27 @@ def get_1clinic_lock(id_unlock = None):
     return c_rec
 
 if __name__ == "__main__":
-    
-    import os    
+
+    import os
     import datetime
 
     log.info("======================= INSB-2 ===========================================")
 
     sout = "Database: {0}:{1}".format(HOST, DB)
     log.info( sout )
-    
+
     if CID_LIST:
 	for clinic_id in cid_list:
 	    try:
 		mcod = modb.moCodeByMisId(clinic_id)
-		sout = "clinic_id: {0} MO Code: {1}".format(clinic_id, mcod) 
+		sout = "clinic_id: {0} MO Code: {1}".format(clinic_id, mcod)
 		log.debug(sout)
 	    except:
 		sout = "Have not got clinic for clinic_id {0}".format(clinic_id)
 		log.warn(sout)
 		mcod = 0
 		continue
-	    
+
 	    pclinic(clinic_id, mcod)
     elif ILIST:
 	c_rec  = get_1clinic_lock()
@@ -436,13 +436,13 @@ if __name__ == "__main__":
 	    _id = c_rec[0]
 	    clinic_id = c_rec[1]
 	    mcod = c_rec[2]
-	    sout = "clinic_id: {0} MO Code: {1}".format(clinic_id, mcod) 
+	    sout = "clinic_id: {0} MO Code: {1}".format(clinic_id, mcod)
 	    log.debug(sout)
 	    if clinic_id is not None:
 		pclinic(clinic_id, mcod)
 	    c_rec  = get_1clinic_lock(_id)
     else:
-	if MLIST: 
+	if MLIST:
 	    dbmy = DBMY()
 	    clist = get_clist(dbmy)
 	    mcount = len(clist)
@@ -452,18 +452,18 @@ if __name__ == "__main__":
 	    try:
 		mo = modb[mcod]
 		clinic_id = mo.mis_code
-		sout = "clinic_id: {0} MO Code: {1}".format(clinic_id, mcod) 
+		sout = "clinic_id: {0} MO Code: {1}".format(clinic_id, mcod)
 		log.debug(sout)
 	    except:
 		sout = "Have not got clinic for MO Code {0}".format(mcod)
 		log.warn(sout)
 		clinic_id = 0
 		continue
-	    
+
 	    pclinic(clinic_id, mcod)
 	    if MLIST:
 		register_cdone(dbmy, clinic_id)
-	
+
     if MLIST:
 	dbmy.close()
     sys.exit(0)
