d3cf88fc2219159f30072abe71c8b8fff05ad899
Nikolay Gorlov <gnv@ctmed.ru>
2014-09-26 10:44:32 +0700

    parameters adjusted

--- insr-1b.py
+++ insr-1b.py
@@ -29,7 +29,7 @@ DB   = "DBMIS"
 
 CLINIC_OGRN = u""
 
-FNAME = "SM{0}T22_14081.csv"
+FNAME = "SM{0}T22_14091.csv"
 FPATH = "./SM"
 
 STEP = 1000
@@ -59,7 +59,7 @@ ALL_PEOPLE = True # Do IBR for all patients or for DVN candidates only
 
 NO_ENP     = False # Do IBR only for patients without ENP
 #DATE_RANGE = None
-DATE_RANGE = ["2014-08-01","2014-08-31"] 
+DATE_RANGE = ["2014-09-01","2014-09-30"] 
 
 REGISTER_DONE = True
 
