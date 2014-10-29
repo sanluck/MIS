ac3653d1bc1482b67ba81ea4e1f4a15ee4a0e17d
Nikolay Gorlov <gnv@ctmed.ru>
2014-10-29 11:53:49 +0600

    parameters setting

--- insb-2.py
+++ insb-2.py
@@ -55,7 +55,7 @@ OCATO      = '01000'
 PRINT2     = False
 
 #DATE_RANGE = None
-DATE_RANGE = ['2014-09-01','2014-09-30']
+DATE_RANGE = ['2014-10-01','2014-10-31']
 PRINT_ALL  = True # include all patients into MO files
 
 def get_clist(db):
