c7c577ddd06a17ab1f7258e9ec136f371a56c847
Nikolay Gorlov <gnv@ctmed.ru>
2014-12-18 16:01:02 +0600

    set ADATE_ATT

--- PatientInfo.py
+++ PatientInfo.py
@@ -27,7 +27,7 @@ DOC_TYPES = {1:u"1",
 SKIP_OGRN  = True # Do not put OGRN into IBR
 
 ASSIGN_ATT = True
-ADATE_ATT  = '20141101'
+ADATE_ATT  = '20141201'
 
 STEP = 1000
 log = logging.getLogger(__name__)
