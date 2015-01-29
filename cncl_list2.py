672068ad4020d61e0d18b3eb2676fcc8794cd3f8
Nikolay Gorlov <gnv@ctmed.ru>
2015-01-29 18:57:53 +0600

    use parameters, save results to xls file

--- cncl_list2.py
+++ cncl_list2.py
@@ -6,7 +6,9 @@
 #
 
 import logging
+import os
 import sys, codecs
+import ConfigParser
 
 from dbmysql_connect import DBMY
 
@@ -21,13 +23,59 @@ logging.getLogger('').addHandler(console)
 
 log = logging.getLogger(__name__)
 
-HOST = "fb2.ctmed.ru"
-DB   = "DBMIS"
+Config = ConfigParser.ConfigParser()
+PATH = os.getcwd()
+FINI = PATH + "/" + "cncl_list.ini"
 
-D_START = "2014-11-01"
-D_END = "2014-11-30"
+from ConfigSection import ConfigSectionMap
+# read INI data
+Config.read(FINI)
+# [DBMIS]
+Config1 = ConfigSectionMap(Config, "DBMIS")
+HOST = Config1['host']
+DB = Config1['db']
 
-CNCL_MAX = 10
+sout = "Database: {0}:{1}".format(HOST, DB)
+log.info(sout)
+
+# [CNCL_COUNT]
+Config2 = ConfigSectionMap(Config, "CNCL_COUNT")
+D_START = Config2['d_start']
+D_FINISH = Config2['d_finish']
+C_METHOD = int(Config2['c_method'])
+# C_METHOD: метод расчета
+# 1 - учитывать только "Результаты осмотра"
+# 2 - учитывать только "Рекомендации"
+# иначе - учитывать и "Результаты осмотра" и "Результаты осмотра"
+#         (должно быть заполнено что-то одно из двух)
+#
+
+sout = "Диапазон дат: [{0}] - [{1}]".format(D_START, D_FINISH)
+log.info( sout )
+
+log.info("Метод расчета:")
+if C_METHOD == 1:
+    sout = '1 - учитывать только "Результаты осмотра"'
+elif C_METHOD == 2:
+    sout = '2 - учитывать только "Рекомендации"'
+else:
+    sout = 'учитывать и "Результаты осмотра" и "Результаты осмотра"'
+
+log.info( sout )
+
+CNCL_MAX = int(Config2['cncl_max'])
+# CNCL_MAX: максимальное количесвто обнаруженных заполненных заключений
+# после чего расчет по текущей клинике завершается
+# если поставить ноль или меньше, то будут просчитаны все заключения
+#
+
+NEW_COUNT = int(Config2['new_count'])
+# Если = 1, то расчет с начала по всем клиникам
+# иначе - продолжаем расчет
+
+FNAME = Config2['fname']
+# имя файла для записи результата
+#
 
 SQLT_TCOUNT = """SELECT count(t.ticket_id)
 FROM tickets t
@@ -44,6 +92,10 @@ AND c.ms_uid is not Null;"""
 
 MY_DB = "web2py_mis"
 MY_DBHOST = "ct216.ctmed.ru"
+
+SQLT_NEWCOUNT = """UPDATE cncl_clinics
+SET cncl_count = Null
+WHERE id > 0;"""
 
 SQLT_GLIST = """SELECT
 id, clinic_id, name, t_count
@@ -64,7 +116,7 @@ WHERE id = %s;"""
 
 STEP = 1000
 
-def get_uid_list(clinic_id, d_start = D_START, d_end = D_END):
+def get_uid_list(clinic_id, d_start = D_START, d_end = D_FINISH):
     import fdb
     from datetime import datetime
     from dbmis_connect2 import DBMIS
@@ -98,6 +150,45 @@ def get_uid_list(clinic_id, d_start = D_START, d_end = D_END):
 
     return c_arr, t_count
 
+def write_result(fname = FNAME):
+    import xlwt
+
+    dbmy = DBMY(host = MY_DBHOST, db = MY_DB)
+    cur = dbmy.con.cursor()
+    ssql = "SELECT * FROM cncl_clinics;"
+    cur.execute(ssql)
+    result = cur.fetchall()
+
+    wb = xlwt.Workbook(encoding='cp1251')
+    ws = wb.add_sheet('Conclusions Count')
+
+    ws.write(0,0,"clinic_id")
+    ws.write(0,1,"mcod")
+    ws.write(0,2,"name")
+    ws.write(0,3,"cncl_count")
+    ws.write(0,4,"t_count")
+    ws.write(0,5,"uid_count")
+
+    row = 1
+    for rec in result:
+        clinic_id = rec[1]
+        mcod = rec[2]
+        name = rec[3]
+        cncl_count = rec[4]
+        t_count = rec[5]
+        uid_count = rec[6]
+
+        row += 1
+
+        ws.write(row,0,clinic_id)
+        ws.write(row,1,mcod)
+        ws.write(row,2,name)
+        ws.write(row,3,cncl_count)
+        ws.write(row,4,t_count)
+        ws.write(row,5,uid_count)
+
+    wb.save(fname)
+    dbmy.close()
 
 if __name__ == "__main__":
     import time
@@ -111,6 +202,14 @@ if __name__ == "__main__":
     curm = dbmy.con.cursor()
 
     # cncl_list = get_cncl_list()
+
+    if NEW_COUNT == 1:
+    # Если = 1, то расчет с начала по всем клиникам
+        curm.execute(SQLT_NEWCOUNT)
+        dbmy.con.commit()
+        log.info("Расчет с начала по всем клиникам")
+    else:
+        log.info("Продолжение расчета по клиникам")
 
     cncl_list = []
 
@@ -149,9 +248,14 @@ if __name__ == "__main__":
             s2 = cncl[1]
             s3 = cncl[2]
 
-            if (len(s3) > 2) or (len(s1) > 2) :
-                cncl_count += 1
-                if (CNCL_MAX > 0) and (cncl_count > CNCL_MAX): break
+            if C_METHOD == 1:
+                if len(s1) > 2 : cncl_count += 1
+            elif C_METHOD == 2:
+                if len(s3) > 2 : cncl_count += 1
+            else:
+                if (len(s3) > 2) or (len(s1) > 2) : cncl_count += 1
+
+            if (CNCL_MAX > 0) and (cncl_count > CNCL_MAX): break
 
             i += 1
             if i % STEP == 0: print i, cncl_count, l_uid
@@ -163,6 +267,7 @@ if __name__ == "__main__":
         dbmy.con.commit()
 
     dbmy.close()
+    write_result()
     localtime = time.asctime( time.localtime(time.time()) )
     log.info('Get Conclusions Count. Finish  '+localtime)
 
