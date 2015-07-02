c61bf9cd97a66c7074ed75f182ba4a5639effb3d
Nikolay Gorlov <gnv@ctmed.ru>
2015-07-02 12:27:21 +0600

    report 2019: work in progress

--- report-2019.py
+++ report-2019.py
@@ -73,6 +73,20 @@ LEFT JOIN cause_death cd ON adc.id_document = cd.id_document
 LEFT JOIN sprav_mkb mkb ON cd.id_mkb = mkb.id_mkb
 WHERE d.datetime BETWEEN %s AND %s AND i.birthday is not Null;"""
 
+SQLT2 = """SELECT
+ap.area_people_id, ap.area_id_fk, ap.date_beg, ap.motive_attach_beg_id_fk,
+ca.clinic_id_fk,
+ar.area_number, ar.area_id,
+ca.speciality_id_fk
+FROM area_peoples ap
+LEFT JOIN areas ar ON ap.area_id_fk = ar.area_id
+LEFT JOIN clinic_areas ca ON ar.clinic_area_id_fk = ca.clinic_area_id
+WHERE ap.people_id_fk = ?
+AND ca.basic_speciality = 1
+AND ca.speciality_id_fk IN (1,7,38,51)
+AND ap.date_end is Null
+ORDER BY ap.date_beg DESC;"""
+
 class D_CASE:
     def __init__(self):
         self.people_id = None
@@ -86,6 +100,7 @@ class D_CASE:
         self.clinic_id = None
         self.mcod      = None
         self.area_number = None
+        self.speciality_id = None
         self.worker_id = None
         self.doctor_id = None
         self.doctor_fio = None
@@ -154,6 +169,33 @@ def set_people_id(cur, d):
     else:
         return None
 
+def set_people_area(cur, d):
+    
+    people_id = d.people_id
+    
+    if people_id is None: return None
+    
+    cur.execute(SQLT2, (people_id, ))
+    results = cur.fetchall()
+    
+    if (results is None) or (len(results)==0): return None
+    
+    rec = results[0]
+    area_people_id = rec[0]
+    area_id_fk = rec[1]
+    date_beg = rec[2]
+    motive_attach_beg_id_fk  =rec[3]
+    clinic_id = rec[3]
+    area_number = rec[4]
+    area_id = rec[5]
+    speciality_id = rec[6]
+    
+    d.clinic_id = clinic_id
+    d.area_number = area_number
+    d.speciality_id = speciality_id
+    
+    return len(results)
+
 def find_peoples(ddd):
     import fdb
     from dbmis_connect2 import DBMIS
@@ -168,16 +210,31 @@ def find_peoples(ddd):
     # and cursor
     ro_cur = ro_transaction.cursor()
     
-    nnf = 0
-    nfd = 0
+    nnf = 0 # number of not found peoples
+    nfd = 0 # number of found duplicated peoples
+    nna = 0 # number of peoples without area_number
+    nda = 0 # number of peoples having more than one area number
+    
     for d_key in ddd.keys():
         d = ddd[d_key]
+        
         nfound = set_people_id(ro_cur, d)
         
         if nfound:
+            ddd[d_key] = d
             if nfound > 1: nfd += 1
         else:
             nnf += 1
+            continue
+        
+        nap = set_people_area(ro_cur, d)
+        if nap:
+            ddd[d_key] = d
+            if nap > 1: nda += 1
+        else:
+            nna += 1
+
+    dbc.close()
     
     sout = "Not found peoples: {0}".format(nnf)
     log.info(sout)
@@ -185,7 +242,11 @@ def find_peoples(ddd):
     sout = "Found duplicates: {0}".format(nfd)
     log.info(sout)
     
-    dbc.close()
+    sout = "Number of peoples without area_number: {0}".format(nna)
+    log.info(sout)
+    
+    sout = "Number of peoples having more than one area number: {0}".format(nda)
+    log.info(sout)
         
 if __name__ == "__main__":
 
@@ -212,4 +273,6 @@ if __name__ == "__main__":
     
     find_peoples(d_dict)
     
+    localtime = time.asctime( time.localtime(time.time()) )
+    log.info('Report 2019 Finish {0}'.format(localtime))
     sys.exit(0)
