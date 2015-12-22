d2519f989ef607065703cc5449f88f5c26ab4b7c
Nikolay Gorlov <gnv@ctmed.ru>
2015-12-22 12:01:19 +0600

    reso-2 implemented

--- reso-2.py
+++ reso-2.py
@@ -11,7 +11,7 @@ import sys, codecs
 
 sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 
-LOG_FILENAME = '_reso1.out'
+LOG_FILENAME = '_reso2.out'
 logging.basicConfig(filename=LOG_FILENAME,level=logging.INFO,)
 
 console = logging.StreamHandler()
@@ -25,6 +25,7 @@ DB   = "DBMIS"
 
 CLINIC_ID = 22
 INSORG_ID = 8
+OMS_SER = u"АК%"
 
 F_PATH    = "./RESO/"
 FOUT_NAME = "ims_out.xls"
@@ -35,7 +36,7 @@ s_sqlt0 = """SELECT
 people_id, lname, fname, mname, birthday 
 FROM peoples 
 WHERE insorg_id = ? 
-AND medical_insurance_series like 'АК%';"""
+AND medical_insurance_series like ?;"""
 
 s_sqlt1 = """SELECT
 document_type_id_fk, document_series, document_number, insurance_certificate,
@@ -74,7 +75,7 @@ phone, mobile_phone
 FROM peoples
 WHERE people_id = ?"""
 
-def get_plist(insorg_id=INSORG_ID):
+def get_plist(insorg_id=INSORG_ID, oms_ser=OMS_SER):
     import fdb
     from dbmis_connect2 import DBMIS
     from people import PEOPLE
@@ -89,7 +90,7 @@ def get_plist(insorg_id=INSORG_ID):
 
     plist = []
 
-    ro_cur.execute(s_sqlt0, (insorg_id,))
+    ro_cur.execute(s_sqlt0, (insorg_id, oms_ser, ))
     results = ro_cur.fetchall()
     
     for rec in results:
@@ -103,7 +104,10 @@ def get_plist(insorg_id=INSORG_ID):
         p.people_id = people_id
         p.lname    = lname.strip()
         p.fname    = fname.strip()
-        p.mname    = mname.strip()
+        if mname:
+            p.mname    = mname.strip()
+        else:
+            p.mname    = None
         p.birthday = birthday
         
         plist.append(p)
@@ -119,6 +123,7 @@ if __name__ == "__main__":
     
     import xlwt
     
+    import fdb
     from dbmis_connect2 import DBMIS
     from people import get_people
 
@@ -126,17 +131,21 @@ if __name__ == "__main__":
     log.info( sout )
     clinic_id = CLINIC_ID
 
+    sout = "INSORG_ID: {0} OMS_SER: {1}".format(INSORG_ID, OMS_SER.encode("utf-8"))
+    log.info( sout )
+
     plist = get_plist()
     l_plist = len(plist)
+
     sout = "Have got {0} peoples matching criteria".format(l_plist)
     log.info( sout )
 
-    if True:
-        sys.exit(0)
-    
     dbc  = DBMIS(clinic_id, mis_host = HOST, mis_db = DB)
-    curr = dbc.con.cursor()
-    cur2 = dbc.con.cursor()
+
+    # Create new READ ONLY READ COMMITTED transaction
+    ro_transaction = dbc.con.trans(fdb.ISOLATION_LEVEL_READ_COMMITED_RO)
+    # and cursor
+    ro_cur = ro_transaction.cursor()
 
     f_fname = F_PATH + FOUT_NAME    
     
@@ -170,98 +179,96 @@ if __name__ == "__main__":
         fname = p.fname
         mname = p.mname
         dr    = p.birthday
+        p_id  = p.people_id
         
-        results = get_people(curr, lname, fname, mname, dr)
-        for rec in results:
-            people_id = rec[0]
-            cur2.execute(s_sqlt3,(people_id,))
-            rfs = cur2.fetchall()
-            for rf in rfs:
-                medical_insurance_series = rf[0]
-                medical_insurance_number = rf[1]
-                document_type_id_fk      = rf[2]
-                document_series          = rf[3]
-                document_number          = rf[4]
-                document_when            = rf[5]
-                insurance_certificate    = rf[6]
-                addr_jure_town_name      = rf[7]
-                addr_jure_town_socr      = rf[8]
-                addr_jure_area_name      = rf[9]
-                addr_jure_area_socr      = rf[10]
-                addr_jure_country_name   = rf[11]
-                addr_jure_country_socr   = rf[12]
-                addr_jure_street_name    = rf[13]
-                addr_jure_street_socr    = rf[14]
-                addr_jure_house          = rf[15]
-                addr_jure_flat           = rf[16]
-                phone                    = rf[17]
-                mobile_phone             = rf[18]
+        ro_cur.execute(s_sqlt3,(p_id,))
+        rfs = ro_cur.fetchall()
+        for rf in rfs:
+            medical_insurance_series = rf[0]
+            medical_insurance_number = rf[1]
+            document_type_id_fk      = rf[2]
+            document_series          = rf[3]
+            document_number          = rf[4]
+            document_when            = rf[5]
+            insurance_certificate    = rf[6]
+            addr_jure_town_name      = rf[7]
+            addr_jure_town_socr      = rf[8]
+            addr_jure_area_name      = rf[9]
+            addr_jure_area_socr      = rf[10]
+            addr_jure_country_name   = rf[11]
+            addr_jure_country_socr   = rf[12]
+            addr_jure_street_name    = rf[13]
+            addr_jure_street_socr    = rf[14]
+            addr_jure_house          = rf[15]
+            addr_jure_flat           = rf[16]
+            phone                    = rf[17]
+            mobile_phone             = rf[18]
                 
-                row += 1
+            row += 1
                 
-                if row % STEP == 0:
+            if row % STEP == 0:
+                
+                sout = "{0}/{1} {2} {3} {4} {5}".format(row, counta, p_id, lname.encode('utf-8'), fname.encode('utf-8'), mname.encode('utf-8'))
+                log.info( sout )
+
+            ws.write(row,0,lname)
+            ws.write(row,1,fname)
+            ws.write(row,2,mname)
+            s_dr = u"%04d-%02d-%02d" % (dr.year, dr.month, dr.day)
+            ws.write(row,3,s_dr)
+
+            ws.write(row,4,medical_insurance_series)
+            ws.write(row,5,medical_insurance_number)
+            ws.write(row,6,insurance_certificate)
+            ws.write(row,7,document_type_id_fk)
+            ws.write(row,8,document_series)
+            ws.write(row,9,document_number)
+                
+            if document_when is None:
+                s_dw = ''
+            else:
+                s_dw = u"%04d-%02d-%02d" % (document_when.year, document_when.month, document_when.day)
                     
-                    sout = "{0}/{1} {2} {3} {4} {5}".format(row, counta, people_id, lname.encode('utf-8'), fname.encode('utf-8'), mname.encode('utf-8'))
-                    log.info( sout )
+            ws.write(row,10,s_dw)
 
-                ws.write(row,0,lname)
-                ws.write(row,1,fname)
-                ws.write(row,2,mname)
-                s_dr = u"%04d-%02d-%02d" % (dr.year, dr.month, dr.day)
-                ws.write(row,3,s_dr)
+            ws.write(row,11,phone)
+            ws.write(row,12,mobile_phone)
 
-                ws.write(row,4,medical_insurance_series)
-                ws.write(row,5,medical_insurance_number)
-                ws.write(row,6,insurance_certificate)
-                ws.write(row,7,document_type_id_fk)
-                ws.write(row,8,document_series)
-                ws.write(row,9,document_number)
+            
+            addr = u''
                 
-                if document_when is None:
-                    s_dw = ''
+            if addr_jure_town_name is not None:
+                addr += addr_jure_town_name 
+                if addr_jure_town_socr is not None:
+                    addr += u' ' + addr_jure_town_socr + u', '
                 else:
-                    s_dw = u"%04d-%02d-%02d" % (document_when.year, document_when.month, document_when.day)
-                    
-                ws.write(row,10,s_dw)
+                    addr += u', '
+            if addr_jure_area_name is not None:
+                addr += addr_jure_area_name
+                if addr_jure_area_socr is not None:
+                    addr += u' ' + addr_jure_area_socr + u', '
+                else:
+                    addr += u', '
+            if addr_jure_country_name is not None:
+                addr += addr_jure_country_name
+                if addr_jure_country_socr is not None:
+                    addr += u' ' + addr_jure_country_socr + u', '
+                else:
+                    addr += u', '
+                
+            if addr_jure_street_name is not None:
+                addr += addr_jure_street_name
+                if addr_jure_street_socr is not None:
+                    addr += u' ' + addr_jure_street_socr
+                
+            if addr_jure_house is not None:
+                addr += u', дом ' + addr_jure_house
 
-                ws.write(row,11,phone)
-                ws.write(row,12,mobile_phone)
-
+            if addr_jure_flat is not None:
+                addr += u', кв. ' + addr_jure_flat
                 
-                addr = u''
-                
-                if addr_jure_town_name is not None:
-                    addr += addr_jure_town_name 
-                    if addr_jure_town_socr is not None:
-                        addr += u' ' + addr_jure_town_socr + u', '
-                    else:
-                        addr += u', '
-                if addr_jure_area_name is not None:
-                    addr += addr_jure_area_name
-                    if addr_jure_area_socr is not None:
-                        addr += u' ' + addr_jure_area_socr + u', '
-                    else:
-                        addr += u', '
-                if addr_jure_country_name is not None:
-                    addr += addr_jure_country_name
-                    if addr_jure_country_socr is not None:
-                        addr += u' ' + addr_jure_country_socr + u', '
-                    else:
-                        addr += u', '
-                
-                if addr_jure_street_name is not None:
-                    addr += addr_jure_street_name
-                    if addr_jure_street_socr is not None:
-                        addr += u' ' + addr_jure_street_socr
-                
-                if addr_jure_house is not None:
-                    addr += u', дом ' + addr_jure_house
-
-                if addr_jure_flat is not None:
-                    addr += u', кв. ' + addr_jure_flat
-                
-                ws.write(row,13,addr)
-                
+            ws.write(row,13,addr)
+        
     
     dbc.close()
     wb.save(f_fname)
