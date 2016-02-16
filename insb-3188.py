b0dc42859ac882a5b8c20f7e61f642a08b9353f9
Nikolay Gorlov <gnv@ctmed.ru>
2016-02-16 10:13:43 +0600

    insb-3188 started to implement

--- insb-3188.py
+++ insb-3188.py
@@ -14,6 +14,8 @@ import sys, codecs
 import ConfigParser
 import logging
 
+import fdb
+from dbmis_connect2 import DBMIS
 from dbmysql_connect import DBMY
 
 from medlib.moinfolist import MoInfoList
@@ -87,8 +89,54 @@ STEP = 1000
 
 OCATO      = '01000'
 
-SQLT_GETP = """
-"""
+SQLT_GET_PEOPLES = """SELECT
+people_id, birthday, enp,
+medical_insurance_series,
+medical_insurance_number
+FROM peoples;"""
+
+SQLT_AP = """SELECT
+ap.area_people_id, ap.area_id_fk, ap.date_beg, ap.motive_attach_beg_id_fk,
+ca.clinic_id_fk,
+ar.area_number,
+ca.speciality_id_fk,
+ap.docum_attach_beg_number
+FROM area_peoples ap
+LEFT JOIN areas ar ON ap.area_id_fk = ar.area_id
+LEFT JOIN clinic_areas ca ON ar.clinic_area_id_fk = ca.clinic_area_id
+WHERE ap.people_id_fk = ?
+AND ca.basic_speciality = 1
+AND ca.speciality_id_fk IN (1,7,38,51)
+AND ap.date_end is Null
+ORDER BY ap.date_beg DESC;"""
+
+SQLT_GETP0 = """SELECT
+people_id, fname, lname, mname, birthday, enp,
+medical_insurance_series,
+medical_insurance_number
+FROM peoples
+WHERE enp = ?;"""
+
+class PEOPLE:
+    def __init__(self):
+        self.people_id = None
+        self.fname = None
+        self.lname = None
+        self.mname = None
+        self.birthday = None
+        self.enp = None
+        self.oms_ser = None
+        self.oms_num = None
+        
+    def init_from_rec(self, rrr):
+        self.people_id = rrr[0]
+        self.fname = rrr[1]
+        self.lname = rrr[2]
+        self.mname = rrr[3]
+        self.birthday = rrr[4]
+        self.enp = rrr[5]
+        self.oms_ser = rrr[6]
+        self.oms_num = rrr[7]
 
 def get_fnames(path = IN_PATH, file_ext = '.csv'):
 # get file names
@@ -104,15 +152,72 @@ def get_fnames(path = IN_PATH, file_ext = '.csv'):
 
     return fnames
 
+def get_peoples():
+    
+    log.info('GET ALL PEOPLES. Start {0}'.format(localtime))
+    sout = "Database: {0}:{1}".format(HOST, DB)
+    log.info(sout)
+
+    p_enp = {}
+    p_oms = {}
+
+
+    try:
+        dbc = DBMIS(mis_host = HOST, mis_db = DB)
+    except:
+        log.warn("Can't connect to DBMIS")
+        return p_enp, p_oms
+
+    # Create new READ ONLY READ COMMITTED transaction
+    ro_transaction = dbc.con.trans(fdb.ISOLATION_LEVEL_READ_COMMITED_RO)
+    # and cursor
+    cur = ro_transaction.cursor()
+
+    cur.execute(SQLT_GET_PEOPLES)
+    results = cur.fetchall()
+    for rec in results:
+        people = PEOPLE()
+        people.people_id = rec[0]
+        people.birthday = rec[1]
+        people.enp = rec[2]
+        oms_ser = rec[3]
+        if oms_ser:
+            people.oms_ser = oms_ser.strip()
+        else:
+            people.oms_ser = None
+        oms_num = rec[4]
+        if oms_num:
+            people.oms_num = oms_num.strip()
+            if (not people.enp) and len(people.oms_num) == 16:
+                people.enp = people.oms_num
+        else:
+            people.oms_num = None
+
+        if people.enp: p_enp[people.enp] = people
+        if people.oms_ser:
+            oms_sn = people.oms_ser
+        else:
+            oms_sn = u""
+
+        if people.oms_num:
+            oms_sn += people.oms_num
+
+        if len(oms_sn) > 0: p_oms[oms_sn] = people
+        
+    dbc.con.commit()
+    dbc.close()
+    
+    log.info('GET ALL PEOPLES. Finish {0}'.format(localtime))
+    log.info('len(p_enp): {0} len(p_oms): {1}'.format(len(p_enp), len(p_oms)))
+    return p_enp, p_oms
+
 def get_plist(f_fname):
 # get patients list
-    import fdb
-    from dbmis_connect2 import DBMIS
     from PatientInfo import PatientInfo
     import time
 
     localtime = time.asctime( time.localtime(time.time()) )
-    log.info('Get Patients List on the Base of {0}'.format(f_fname))
+    log.info('Get Patients List on the Base of {0}. Start {1} '.format(f_fname, localtime))
 
     # read file line by line into arry
     
@@ -143,9 +248,29 @@ def get_plist(f_fname):
     # and cursor
     ro_cur = ro_transaction.cursor()
     
+    nnn = 0
+    ppp_arr = []
     for enp in array:
         
+        ro_cur.execute(SQLT_GETP0, (enp, ))
+        rec = ro_cur.fetchone()
+        if not rec: continue
 
+        ppp = PEOPLE()
+        ppp.init_from_rec(rec)
+        
+        ppp_arr.append(ppp)
+        nnn += 1
+        
+    dbc.con.commit()
+    dbc.close()
+
+    localtime = time.asctime( time.localtime(time.time()) )
+    log.info('Get Patients List. Finish {0}'.format(localtime))
+    
+    log.info('Have found {0} peoples in DBMIS'.format(nnn))
+    
+    return ppp_arr
 
 if __name__ == "__main__":
 
@@ -155,6 +280,8 @@ if __name__ == "__main__":
     log.info("======================= INSB-3188 ===========================================")
     localtime = time.asctime( time.localtime(time.time()) )
     log.info('Getting patients and processing data. Start {0}'.format(localtime))
+
+    # p_enp, p_oms = get_peoples()
 
     fnames = get_fnames()
     n_fnames = len(fnames)
